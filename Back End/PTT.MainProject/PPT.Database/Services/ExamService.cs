e8bb4ce939b61ea5ef53eb562f601dc21bc1a7e6
hammer1910 <lamsaodayhehe@gmail.com>
2019-03-13 13:03:52 +0700

    create classes

---
+++ Back End/PTT.MainProject/PPT.Database/Services/ExamService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PPT.Database.Services
+{
+    public class ExamService
+    {
+
+    }
+}
---
+++ Back End/PTT.MainProject/PTT.MainProject/Controllers/ExamController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PTT.MainProject.Controllers
+{
+    [Route("api/exam")]
+    public class ExamController : Controller
+    {
+
+    }
+}
