734c0f7e069ac312d8964e28b6ffd4a8ca561ed5
hammer1910 <lamsaodayhehe@gmail.com>
2019-03-13 13:47:30 +0700

    add filed correctanswer

---
+++ Back End/PTT.MainProject/PPT.Database/Migrations/20190313064521_Add Correct-Answer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PPT.Database.Migrations
+{
+    public partial class AddCorrectAnswer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CorrectAnswer",
+                table: "ExamQuestions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CorrectAnswer",
+                table: "Questions",
+                maxLength: 10,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CorrectAnswer",
+                table: "Questions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CorrectAnswer",
+                table: "ExamQuestions",
+                maxLength: 10,
+                nullable: true);
+        }
+    }
+}
