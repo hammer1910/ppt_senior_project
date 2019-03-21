b5d7697bc4577ebfb64739d64889fb2d5bced386
vancanh3 <truongvancanhdtu@gmail.com>
2019-03-21 14:50:51 +0700

    adam

---
+++ Back End/PTT.MainProject/PPT.Database/Common/Pagging.cs
@@ -0,0 +1,22 @@
+﻿using PPT.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using PPT.Database.Services;
+
+namespace PPT.Database.Common
+{
+    public class Pagging 
+    {
+        public static List<QuestionEntity> GetQuestions(int page, List<QuestionEntity> questionEntities)
+        {
+            List<QuestionEntity> questionsList = new List<QuestionEntity>();
+            int start = (page - 1) * 3;
+            for (int i = start; i < start + 3; i++)
+            {
+                questionsList.Add(questionEntities[i]);
+            }
+            return questionsList;
+        }
+    }
+}
